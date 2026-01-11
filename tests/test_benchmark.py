9abc00916e379dc836fd991972eb7fbe58cbda38
automatedtomato <tomizawa.licht@gmail.com>
2026-01-11 09:20:51 +0000

    setup ci

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,12 +1,14 @@
 import subprocess
 
 import pytest
+import torch
 
 config_paths = ["src/configs/benchmark_config.yaml"]
 
 
 @pytest.mark.parametrize("config_path", config_paths)
-def test_benchmark(config_path: str) -> None:
+@pytest.mark.skipif(not torch.cuda.is_available(), reason="GPU not available")
+def benchmark_test_gpu(config_path: str) -> None:
     """Test that bechmark script is excutable."""
     cmd = [
         "python3",
